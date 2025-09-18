c3a501119eb6cc22da2df76ee43dc4a0092cdeed
Camilo Zuliaga <v-czuliaga@youngliving.com>
2025-09-18 16:56:11 -0500

    mongo db added

--- RealEstateApp.AppHost/Program.cs
+++ RealEstateApp.AppHost/Program.cs
@@ -1,12 +1,20 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.RealEstateApp_ApiService>("weatherapi");
+var mongo = builder.AddMongoDB("mongo")
+                   .WithMongoExpress();
+
+var mongodb = mongo.AddDatabase("mongodb");
+
+var apiService = builder.AddProject<Projects.RealEstateApp_ApiService>("weatherapi")
+    .WithReference(mongodb)
+    .WaitFor(mongodb);
 
 builder.AddNpmApp("react", "../RealEstateApp.ReactApp")
     .WithReference(apiService)
+    .WaitFor(apiService)
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile();
+    .PublishAsDockerFile() ;
 
 builder.Build().Run();
